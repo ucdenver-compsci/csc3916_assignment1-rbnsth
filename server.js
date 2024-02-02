789410e31a2861e84e9057ea574210e0524f92da
Robin Shrestha <57116623+rbnsth@users.noreply.github.com>
2024-02-02 04:29:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
 const app = express();
 
-// Middleware to parse POST body as JSON
-app.use(express.json());
+app.use(bodyParser.text({
+    type: function(req) {
+        return 'text';
+    }
+}));
 
+
+// Parse bodies of POST requests
 app.post('/', (req, res) => {
     console.log(req.body);
+    res = res.status(200);
+    var contentType = req.get('content-type');
+    if (contentType){
+        console.log("Content-Type: " + contentType);
+        res = res.type(contentType);
+    }
     res.send(req.body);
 });
 
-const port = process.env.PORT || 8080;
+// Start the server
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
